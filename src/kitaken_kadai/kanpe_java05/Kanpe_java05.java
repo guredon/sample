61708be43681ef0f79302618056ffb549c61669c
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 16:13:15 +0900

    Kadai_05

---
+++ src/kitaken_kadai/kanpe_java05/Kanpe_java05.java
@@ -0,0 +1,16 @@
+package kitaken_kadai.kanpe_java05;
+
+public class Kanpe_java05 {
+
+	public static void main (String[] args){
+		
+		int i,j;
+		
+		for(i=1; i<=9; i++){
+			for(j=1; j<=9; j++){
+				System.out.println(i + "*" + j + "=" + i*j);
+			}
+		}
+	}
+	
+}
