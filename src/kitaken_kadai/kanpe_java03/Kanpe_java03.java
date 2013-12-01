c5a54d9cb475fd9ff75a627b928dc773fbed0f84
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 15:32:35 +0900

    Kadai_03 not for sentence

--- src/kitaken_kadai/kanpe_java03/Kanpe_java03.java
+++ src/kitaken_kadai/kanpe_java03/Kanpe_java03.java
@@ -3,8 +3,8 @@ package kitaken_kadai.kanpe_java03;
 public class Kanpe_java03 {
 
 	public static void main(String[] args){	
-		int i;
-		for(i=0; i<=2; i++)
+			System.out.println("こんにちは世界!!\n");
+			System.out.println("こんにちは世界!!\n");
 			System.out.println("こんにちは世界!!\n");
 	}
 }
