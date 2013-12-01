e5700b44797c8a2c69d9af8eae26d7b0fa6b897e
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 15:35:37 +0900

    Kadai_04

---
+++ src/kitaken_kadai/kanpe_java04/Kanpe_java04.java
@@ -0,0 +1,14 @@
+package kitaken_kadai.kanpe_java04;
+
+public class Kanpe_java04 {
+
+	public static void main (String[] args){
+
+		int i;	
+		
+		for(i=0; i<=2; i++)
+			System.out.println("Hello World");
+		
+	}
+	
+}
