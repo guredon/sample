509769a448e8710a15d29b6ed63117e6c46e3483
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 15:14:32 +0900

    Kadai_02

---
+++ src/kitaken_kadai/kanpe_java02/Kanpe_java02.java
@@ -0,0 +1,12 @@
+package kitaken_kadai.kanpe_java02;
+
+public class Kanpe_java02 {
+
+public static void main(String[] args){
+	System.out.print("Javaバージョン(java.version);");
+	System.out.println(System.getProperty("java.version"));
+	
+	System.out.print("オペレーティング・システム名(os.name):");
+	System.out.println(System.getProperty("os.name"));
+}
+}
