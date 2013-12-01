8f4b2f0419b5ab281d2d255540533f8c44bfe3de
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 16:25:51 +0900

    Cosmetic

--- src/kitaken_kadai/kanpe_java05/Kanpe_java05.java
+++ src/kitaken_kadai/kanpe_java05/Kanpe_java05.java
@@ -2,15 +2,12 @@ package kitaken_kadai.kanpe_java05;
 
 public class Kanpe_java05 {
 
-	public static void main (String[] args){
+	public static void main(String[] args) {
 		
-		int i,j;
-		
-		for(i=1; i<=9; i++){
-			for(j=1; j<=9; j++){
+		for (int i=1; i<=9; i++) {
+			for (int j=1; j<=9; j++) {
 				System.out.println(i + "*" + j + "=" + i*j);
 			}
 		}
 	}
-	
 }
