944cc27175380c42a220d5411d6303e1b70c9828
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-01 15:22:03 +0900

    Kadai_03

---
+++ src/kitaken_kadai/kanpe_java03/Kanpe_java03.java
@@ -0,0 +1,10 @@
+package kitaken_kadai.kanpe_java03;
+
+public class Kanpe_java03 {
+
+	public static void main(String[] args){	
+		int i;
+		for(i=0; i<=2; i++)
+			System.out.println("こんにちは世界!!\n");
+	}
+}
