785a5a053caf53a6a14f3bcbceff7f94176d28b8
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-08 14:03:03 +0900

    Cosmetic 行間を詰める

--- src/kitaken_kadai/kanpe_java05/KanpeJava05.java
+++ src/kitaken_kadai/kanpe_java05/KanpeJava05.java
@@ -1,9 +1,7 @@
 package kitaken_kadai.kanpe_java05;
 
 public class KanpeJava05 {
-
 	public static void main(String[] args) {
-		
 		for (int i=1; i<=9; i++) {
 			for (int j=1; j<=9; j++) {
 				System.out.println(i + "*" + j + "=" + i*j);
