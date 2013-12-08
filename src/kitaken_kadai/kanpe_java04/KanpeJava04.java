05b323fdf3c35e97aefd1a4c003b880fe9777820
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-08 14:00:11 +0900

    Cosmetic for sentence

--- src/kitaken_kadai/kanpe_java04/KanpeJava04.java
+++ src/kitaken_kadai/kanpe_java04/KanpeJava04.java
@@ -1,14 +1,8 @@
 package kitaken_kadai.kanpe_java04;
 
 public class KanpeJava04 {
-
 	public static void main (String[] args){
-
-		int i;	
-		
-		for(i=0; i<=2; i++)
+		for(int i=0; i<3; i++)
 			System.out.println("Hello World");
-		
 	}
-	
 }
