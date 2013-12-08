94ea43549a903b15ebfa361a88cd4c9a22895daa
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-08 13:44:18 +0900

    rename kanpeJava02

---
+++ src/kitaken_kadai/kanpe_java02/KanpeJava02.java
@@ -0,0 +1,10 @@
+package kitaken_kadai.kanpe_java02;
+
+public class KanpeJava02 {
+	public static void main(String[] args){
+		System.out.print("Javaバージョン(java.version);");
+		System.out.println(System.getProperty("java.version"));
+		System.out.print("オペレーティング・システム名(os.name):");
+		System.out.println(System.getProperty("os.name"));
+	}
+}
