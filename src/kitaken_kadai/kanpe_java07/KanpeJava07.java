7121affb7939787ffe9e81fb63cfcc2526615459
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-08 14:30:47 +0900

    Kanpe07 aが10未満なら"a < 10です\n"。10以上なら"10 <= aです\n"と出力する

---
+++ src/kitaken_kadai/kanpe_java07/KanpeJava07.java
@@ -0,0 +1,11 @@
+package kitaken_kadai.kanpe_java07;
+
+public class KanpeJava07 {
+	public static void main(String[] args){
+		int a=10;
+		if(a<10)
+			System.out.println("a < 10 です\n");
+		else if(10<=a)
+			System.out.println("10 <= a です\n");
+	}
+}
