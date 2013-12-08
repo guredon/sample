96abae4d2bbfd29a970d2a96879a890e834702aa
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-08 13:31:27 +0900

    rename kanpeJava01

--- src/kitaken_kadai/kanpe_java01/KanpeJava01.java
+++ src/kitaken_kadai/kanpe_java01/KanpeJava01.java
@@ -1,11 +1,7 @@
 package kitaken_kadai.kanpe_java01;
 
-public class Kanpe_java01 {
-
+public class KanpeJava01 {
 	public static void main(String[] args) {
-
 		System.out.println("Hello World!");
-		
 	}
-
 }
