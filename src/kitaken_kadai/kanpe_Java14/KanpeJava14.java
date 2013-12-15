db5055ef47e0fc2d19de00af23dbedabc47903e5
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-15 22:42:14 +0900

    Cosmetic 13,14

--- src/kitaken_kadai/kanpe_Java14/KanpeJava14.java
+++ src/kitaken_kadai/kanpe_Java14/KanpeJava14.java
@@ -4,21 +4,18 @@ public class KanpeJava14 {
 
 	public static void main(String[] args){
 	KanpeJava14 hoge = new KanpeJava14();
-	int n = 12;
+	int n = 11;
 	System.out.println(hoge.isPrime(n));
 	}
 	
 	boolean isPrime(int n) {
 		int count = 0;
-		for (int i=1; i<n+1; i++) {
-			if (n%i == 0) {
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
 				count++; 
 			}
 		}
-		if (count == 2){
-			return true;
-		} else {
-			return false;
-		}
+	boolean result = count == 2;
+	return result;
 	}
 }
--- src/kitaken_kadai/kanpe_java13/KanpeJava13.java
+++ src/kitaken_kadai/kanpe_java13/KanpeJava13.java
@@ -3,28 +3,24 @@ package kitaken_kadai.kanpe_java13;
 public class KanpeJava13 {
 
 	public static void main(String[] args) {
-	KanpeJava13 hoge = new KanpeJava13(); 
-	int x = 30;
-	int y = 10;
-	int z = 20;
-	System.out.println(hoge.getMin(x,y,z));
+		KanpeJava13 hoge = new KanpeJava13(); 
+		int x = 30;
+		int y = 10;
+		int z = 20;
+		int sum = x + y + z;
+		int mid = hoge.getMid(x,y,z,sum);
+		System.out.println(mid);
 	}
+	
+	int getMid(int a, int b, int c, int s) {
+	
+		int large = (a < b)? b : a;
+		int max = (large < c)? c : large;
 
-	int getMin(int a, int b, int c) {
-		if (a < b) {
-			if (a < c) {
-				if (b < c) {
-					return c;
-				} else {
-					return b;
-				}
-			} return a;
-		} else {
-			if (b < c) {
-				return c;
-			} else {
-				return b;
-			}
-		}
+		int small = (a < b)? a : b;
+		int min = (small < c)? small : c;
+
+		int result = s - max - min;
+		return result;
 	}
 }
