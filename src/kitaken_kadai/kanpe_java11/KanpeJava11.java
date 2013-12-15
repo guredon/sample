c213da62559bd7edf00876ffd04bffea40030563
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-15 14:26:41 +0900

    kanpe11 int getMax(int a, int b, int c)メソッドを書け

---
+++ src/kitaken_kadai/kanpe_java11/KanpeJava11.java
@@ -0,0 +1,29 @@
+package kitaken_kadai.kanpe_java11;
+
+public class KanpeJava11 {
+
+	public static void main(String[] args) {
+		KanpeJava11 hoge = new KanpeJava11(); 
+		int x = 20;
+		int y = 30;
+		int z = 10;
+		System.out.println(hoge.getMax(x,y,z));
+	}
+
+	int getMax(int a, int b, int c) {
+		if (a > b) {
+			if (a > c) {
+				return a;
+			} else if (c > b) {
+				return c;
+			} else {
+				return b;
+			}
+		}
+		else
+			if (b > c)
+				return b;
+			else
+				return c;
+	}
+}
