8a6f55335205322b3ddc4662141d8987f45e4dc0
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-15 13:40:55 +0900

    kanpe10 getMax(int a, int b)では大きい方であったが、小さい方を返すgetMin(int a, int b)を書け

---
+++ src/kitaken_kadai/kanpe_java10/KanpeJava10.java
@@ -0,0 +1,20 @@
+package kitaken_kadai.kanpe_java10;
+
+public class KanpeJava10 {
+
+	public static void main(String[] args) {
+		KanpeJava10 hoge = new KanpeJava10(); 
+		int x=10;
+		int y=20;
+		System.out.println(hoge.getMin(x,y));
+	}
+
+	int getMin(int a, int b) {
+		if(a < b)
+			return a;
+		else if(b < a)
+			return b;
+		else
+			return a;
+	}
+}
