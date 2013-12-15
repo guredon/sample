4c1735d01318aa62bd98ae82e2c4793d4a583f4f
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-15 15:59:38 +0900

    kanpe14 渡されたint型の数値が素数かを判定するboolean isPrime(int n)メソッドを書け

---
+++ src/kitaken_kadai/kanpe14/KanpeJava14.java
@@ -0,0 +1,24 @@
+package kitaken_kadai.kanpe14;
+
+public class KanpeJava14 {
+
+	public static void main(String[] args){
+	KanpeJava14 hoge = new KanpeJava14();
+	int n = 12;
+	System.out.println(hoge.isPrime(n));
+	}
+	
+	boolean isPrime(int n) {
+		int count = 0;
+		for (int i=1; i<n+1; i++) {
+			if (n%i == 0) {
+				count++; 
+			}
+		}
+		if (count == 2){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
