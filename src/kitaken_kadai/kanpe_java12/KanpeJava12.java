316cf58c86db65fac256e84132c1fed52f6e81da
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-15 14:35:52 +0900

    kanpe12 int getMin(int a, int b, int c)メソッドを書け

---
+++ src/kitaken_kadai/kanpe_java12/KanpeJava12.java
@@ -0,0 +1,29 @@
+package kitaken_kadai.kanpe_java12;
+
+public class KanpeJava12 {
+
+	public static void main(String[] args) {
+	KanpeJava12 hoge = new KanpeJava12(); 
+	int x = 30;
+	int y = 20;
+	int z = 10;
+	System.out.println(hoge.getMin(x,y,z));
+	}
+
+	int getMin(int a, int b, int c) {
+		if (a < b) {
+			if (a < c) {
+				return a;
+			} else if (c < b) {
+				return c;
+			} else {
+				return b;
+			}
+		}
+		else
+			if (b < c)
+				return b;
+			else
+				return c;
+	}
+}
