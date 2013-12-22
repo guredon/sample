f008be64e431132ca350f98961194378fc542b68
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-22 14:05:41 +0900

    Cosmetic KanpeJava11, KanpeJava12

--- src/kitaken_kadai/kanpe_java11/KanpeJava11.java
+++ src/kitaken_kadai/kanpe_java11/KanpeJava11.java
@@ -7,24 +7,13 @@ public class KanpeJava11 {
 		int x = 20;
 		int y = 30;
 		int z = 10;
-		System.out.println(hoge.getMax(x,y,z));
+		int max = hoge.getMax(x,y,z);
+		System.out.println(max);
 	}
 
 	int getMax(int a, int b, int c) {
-		if (a > b) {
-			if (a > c) {
-				return a;
-			} else if (c > b) {
-				return c;
-			} else {
-				return b;
-			}
-		} else {
-			if (b > c) {
-				return b;
-			} else {
-				return c;
-			}
-		}
-	}
+		int large = (a < b)? b : a;
+		int result = (large < c)? c : large;
+		return result;
+	}	
 }
--- src/kitaken_kadai/kanpe_java12/KanpeJava12.java
+++ src/kitaken_kadai/kanpe_java12/KanpeJava12.java
@@ -3,28 +3,17 @@ package kitaken_kadai.kanpe_java12;
 public class KanpeJava12 {
 
 	public static void main(String[] args) {
-	KanpeJava12 hoge = new KanpeJava12(); 
-	int x = 30;
-	int y = 20;
-	int z = 10;
-	System.out.println(hoge.getMin(x,y,z));
+		KanpeJava12 hoge = new KanpeJava12(); 
+		int x = 20;
+		int y = 10;
+		int z = 30;
+		int min = hoge.getMin(x,y,z);
+		System.out.println(min);
 	}
 
 	int getMin(int a, int b, int c) {
-		if (a < b) {
-			if (a < c) {
-				return a;
-			} else if (c < b) {
-				return c;
-			} else {
-				return b;
-			}
-		} else {
-			if (b < c) {
-				return b;
-			} else {
-				return c;
-			}
-		}
-	}
+		int small = (a < b)? a : b;
+		int result = (small < c)? small : c;
+		return result;
+	}	
 }
