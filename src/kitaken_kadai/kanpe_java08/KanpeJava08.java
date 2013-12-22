c5afd292ec29b717ee09c752928d9b64ba69ebdf
e115715 <e115715@ie.u-ryukyu.ac.jp>
2013-12-22 13:43:23 +0900

    Cosmetic KanpeJava08

--- src/kitaken_kadai/kanpe_java08/KanpeJava08.java
+++ src/kitaken_kadai/kanpe_java08/KanpeJava08.java
@@ -1,16 +1,22 @@
 package kitaken_kadai.kanpe_java08;
 
 public class KanpeJava08 {
-	public static void main (String[] args){
-		for(int i=1; i<=100; i++){
-			if(i%15==0)
-				System.out.println("FizzBuzz");
-			else if(i%3==0)
-				System.out.println("Fizz");
-			else if(i%5==0)
-				System.out.println("Buzz");
-			else
-				System.out.println(i);
+
+	public static void main(String[] args) {
+		KanpeJava08 hoge = new KanpeJava08(); 
+		for (int i = 1; i <= 100; i++) {
+			hoge.FizzBuzz(i);
 		}
+	}
+	
+	void FizzBuzz(int a) {
+		if (a % 15 == 0)
+			System.out.println("FizzBuzz");
+		else if (a % 3 == 0)
+			System.out.println("Fizz");
+		else if(a % 5== 0)
+			System.out.println("Buzz");
+		else
+			System.out.println(a);
 	}
 }
